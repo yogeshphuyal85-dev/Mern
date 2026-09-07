e2f7bd716b1792d20f9cb07d954375890511f416
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-07 13:02:58 +0545

    feat: implement todo component and update App to render it

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import FRONTEND from './components/frntend.jsx'
+import todo from './components/todo.jsx'
 function App() {
   return (
-    <FRONTEND />
+    // <FRONTEND />
+    <todo/>
   )
 }
 
