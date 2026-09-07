50cd7d791f431badeeb5c6aaccb523ffeb90d9d9
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-07 13:11:36 +0545

    feat: add react-router-dom import to App component for routing functionality

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FRONTEND from './components/frntend.jsx'
 import Todo from './components/todo.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
   return (
     // <FRONTEND />
