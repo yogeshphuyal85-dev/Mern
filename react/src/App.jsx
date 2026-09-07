4f849f2cdca2ad285e3c0d7fb396becbf7a9b3aa
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-07 13:16:25 +0545

    feat: refactor App component to implement routing with react-router-dom

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,11 +2,33 @@ import React from 'react'
 import FRONTEND from './components/frntend.jsx'
 import Todo from './components/todo.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-function App() {
-  return (
-    // <FRONTEND />
-    <Todo/>
-  )
-}
 
+const App = () => {
+  
+const route= createBrowserRouter([
+  {
+    path: "/",
+      element: <Todo />,
+      // children: [
+      //   {
+      //     path: "",
+      //     element: <HomePage />,
+      //   }]
+  }
+]);
+return(
+  <>
+  <RouterProvider router={route} />
+  </>
+);
+};
 export default App;
+
+// function App() {
+//   return (
+//     // <FRONTEND />
+//     <Todo/>
+//   )
+// }
+
+// export default App;
