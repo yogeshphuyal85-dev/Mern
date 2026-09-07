001930998ab884465bf333c1e68fea15036a828e
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-07 13:40:47 +0545

    feat: update routing in App component to include FRONTEND and Todo components

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Todo from "./components/todo.jsx";
+import FRONTEND from "./components/frntend.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 const App = () => {
 
   const route = createBrowserRouter([
     {
-      path: "/",
+      path: "/todo",
       element: <Todo />
+    },
+    {
+      path:"/",
+      element:<FRONTEND/>
     }
   ]);
 
