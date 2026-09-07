ab143ff23a8237dd2edb658b7ba4560d69a7a18b
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-07 13:24:24 +0545

    refactor: clean up App component by removing commented code and unnecessary imports

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,19 @@
 import React from 'react'
-import FRONTEND from './components/frntend.jsx'
 import Todo from './components/todo.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = () => {
-  
-const route= createBrowserRouter([
-  {
-    path: "/",
-      element: <Todo />,
-      // children: [
-      //   {
-      //     path: "",
-      //     element: <HomePage />,
-      //   }]
-  }
-]);
-return(
-  <>
-  <RouterProvider router={route} />
-  </>
-);
-};
-export default App;
 
-// function App() {
-//   return (
-//     // <FRONTEND />
-//     <Todo/>
-//   )
-// }
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <Todo />
+    }
+  ])
 
-// export default App;
+  return (
+    <RouterProvider router={route} />
+  )
+}
+
+export default App
