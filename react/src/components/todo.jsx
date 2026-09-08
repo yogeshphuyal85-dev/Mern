f85a23ebf6ee733ea011bd15407dd06b6f3cc4f8
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-08 12:48:47 +0545

    refactor: remove unnecessary blank lines in Todo component

--- react/src/components/todo.jsx
+++ react/src/components/todo.jsx
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 
 function Todo() {
   const [todos, setTodos] = useState([]);
-
   const [showForm, setShowForm] = useState(true);
-
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("");
