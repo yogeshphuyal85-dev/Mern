410767d1c3816dbb1c5903e168ca40d5c57638dc
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-09 13:16:10 +0545

    feat: implement fetchData function to retrieve data from API in Api_practise component

--- react/src/components/api_practise.jsx
+++ react/src/components/api_practise.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-function Api_practise() {
-  return (
-    <div>datfat api</div>
-  )
-}
+const Api_practise = () => {
+    const [value, setValue] = useState([]);
+
+
+    const fetchData = async () => {
+        const vaue = await axios.get("https://fakestoreapi.com/products");
+        console.log(vaue.data);
+        setValue(vaue.data);
+    };
 
 export default Api_practise;
