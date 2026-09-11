367a18f6f9e681f0a02d5cfe5e82cae728cc4cdf
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-11 14:18:39 +0545

    feat: implement User_api component to fetch and display users from API

--- react/src/components/user_api.jsx
+++ react/src/components/user_api.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
 
-function User_api() {
-  return (
-    <div>user_api</div>
-  )
-}
+const User_api = () => {
+    const [value, setValue] = React.useState([]);
+
+    const fetchData = async () => {
+        await axios.get('https://fakestoreapi.com/users').then((res) => {
+            console.log(res.data);
+            setValue(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className="min-h-screen bg-gray-100 p-8">
+            <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
+                Fake Store Users
+            </h1>
+        </div>
+    );
+};
 
 export default User_api;
