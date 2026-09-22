6de37895f1ea710910ca8fe4f55fd3a8355ae1b8
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-22 13:30:04 +0545

    refactor: clean up commented code in getUser function and improve error handling

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,18 +1,38 @@
 import database from "../database/database.js";
 
 export const getUser = (req, res) => {
-    const user={
-        username: "Yogesh Phuyal",
-        email: "yogesh@gmail.com",
-        phone_number: "9800000000",
-        address: "Gothgaun, Morang",
-        role: "user",
-    };
-    console.log(user);
+    // const user={
+    //     username: "Yogesh Phuyal",
+    //     email: "yogesh@gmail.com",
+    //     phone_number: "9800000000",
+    //     address: "Gothgaun, Morang",
+    //     role: "user",
+    // };
+    // console.log(user);
     
-    return res.send(user);
+    // return res.send(user);
+
+    try{
+        const q = "SELECT * FROM users";
+
+        database.query(q, (err, result) => {
+            if (err) {
+                return res.send({
+                    message: "Error while fetching data",
+                    error: err,
+                });
+            }
+            return res.send({
+                message: "Data fetched successfully",
+                data: result});
+            });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
+
+export const getSingleUser 
 
 
 export const postUser = (req, res) => {
