ddcbf34e08fda0f500ff92a9b245c7b1cb83cf4f
yogeshphuyal85-dev <yogeshphuyal85@gmail.com>
2026-09-22 13:40:55 +0545

    feat: update user routes to include specific paths for edit and delete operations

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {editUser, getSingleUser, getUser, postUser} from "../controllers/user.js";
+import {deleteUser, editUser, getSingleUser, getUser, postUser} from "../controllers/user.js";
 
 
 const route = express.Router();
@@ -8,8 +8,8 @@ const route = express.Router();
 route.get("/user", getUser);
 route.post("/post-user",postUser);
 route.get("/user/:id", getSingleUser);
-route.put("/user/:id", editUser);
-route.delete("/user/:id", deleteUser);
+route.put("/edit-user/:id", editUser);
+route.delete("/delete-user/:id", deleteUser);
 
 
 export default route;
